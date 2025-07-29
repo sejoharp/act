5048cce905abe890bbd18c0144065f326761bb90
Joscha <zippelman@web.de>
2025-07-29 22:33:30 +0200

    add flake to compile binary locally
    It ignores the cargo flag lto = true

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "Rust example flake for Zero to Nix";
+
+  inputs = {
+    # Latest stable Nixpkgs
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+    # A helper library for Rust + Nix
+    rust-overlay.url = "https://flakehub.com/f/oxalica/rust-overlay/*";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      manifest = (nixpkgs.lib.importTOML ./Cargo.toml).package;
+
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            # Provides Nixpkgs with a rust-bin attribute for building Rust toolchains
+            rust-overlay.overlays.default
+            # Uses the rust-bin attribute to select a Rust toolchain
+            self.overlays.default
+          ];
+        };
+      });
+    in
+    {
+      overlays.default = final: prev: {
+        # The Rust toolchain used for the package build
+        rustToolchain = final.rust-bin.stable.latest.default;
+      };
+
+      packages = forAllSystems ({ pkgs }: {
+        default =
+          let
+            rustPlatform = pkgs.makeRustPlatform {
+              cargo = pkgs.rustToolchain;
+              rustc = pkgs.rustToolchain;
+            };
+          in
+          rustPlatform.buildRustPackage {
+            name = manifest.name;
+            version = manifest.version;
+            src = ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+          };
+      });
+    };
+}
